6190bd891819712faba8f929e9cb94cf3600b2b9
Mathias Söderberg <mathias.soederberg@gmail.com>
2015-03-20 23:18:25 +0100

    Add debug statement to Shell.exec
    For, you know, debugging purposes. I’d prefer to have some kind of
    logger, but for now this’ll work.

--- lib/tara/shell.rb
+++ lib/tara/shell.rb
@@ -4,6 +4,7 @@ module Tara
   class Shell
     def self.exec(command)
       output = %x(#{command})
+      $stderr.puts(%(#{command}: #{output})) if ENV['TARA_DEBUG']
       unless $?.success?
         raise ExecError, %(Command `#{command}` failed with output: #{output})
       end
